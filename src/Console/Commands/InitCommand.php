11837c6eee3fd4cdff423bb548844d58c9ba44f6
Noël Hagestein <6616996+Neol3108@users.noreply.github.com>
2022-01-07 18:02:50 +0100

    Save CWD and add styling for progress bars

--- src/Console/Commands/InitCommand.php
+++ src/Console/Commands/InitCommand.php
@@ -2,6 +2,7 @@
 
 namespace Recoded\Craftian\Console\Commands;
 
+use Recoded\Craftian\Craftian;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
@@ -11,8 +12,38 @@ class InitCommand extends Command
     protected static $defaultDescription = 'Boilerplate a server';
     protected static $defaultName = 'init';
 
+    protected function assertIsNotInitialized(): void
+    {
+        if (file_exists(Craftian::getCwd() . '/craftian.json')) {
+            throw new \Exception('Craftian already initialized');
+        }
+    }
+
+    /**
+     * @param array<array-key, mixed> $data
+     * @throws \Exception
+     */
+    protected function initializeJson(array $data): void
+    {
+        $file = fopen(Craftian::getCwd() . '/craftian.json', 'a');
+        $data = json_encode($data, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES);
+
+        if ($file === false || $data === false) {
+            throw new \Exception('Couldn\'t initialize Craftian');
+        }
+
+        fwrite($file, $data);
+        fclose($file);
+    }
+
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
+        $this->assertIsNotInitialized();
+
+        $this->initializeJson([
+            'require' => [],
+        ]);
+
         return static::SUCCESS;
     }
 }
--- src/Craftian.php
+++ src/Craftian.php
@@ -4,11 +4,42 @@ namespace Recoded\Craftian;
 
 use Recoded\Craftian\Console\Commands\InitCommand;
 use Symfony\Component\Console\Application;
+use Symfony\Component\Console\Helper\ProgressBar;
 
 class Craftian extends Application
 {
+    protected static string $cwd;
+
     public function boot(): void
     {
+        static::bootCwd();
+
+        ProgressBar::setFormatDefinition(
+            'downloading',
+            'Downloading [%downloadable%] %current%/%max% [%bar%] %percent:3s%%',
+        );
+
+        ProgressBar::setFormatDefinition(
+            'downloading_version',
+            'Downloading [%downloadable% (%version%)] %current%/%max% [%bar%] %percent:3s%%',
+        );
+
         $this->add(new InitCommand());
+    }
+
+    public static function bootCwd(): void
+    {
+        $cwd = getcwd();
+
+        if ($cwd === false) {
+            throw new \Exception('Couldn\'t determine current directory, this is needed for Craftian');
+        }
+
+        static::$cwd = $cwd;
+    }
+
+    public static function getCwd(): string
+    {
+        return static::$cwd;
     }
 }
